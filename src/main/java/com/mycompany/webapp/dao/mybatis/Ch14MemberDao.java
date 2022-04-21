5ad50e2850166451e06e1ccdaaaa8c71cfbaa6e1
Seongjoo-Shin <ssj3357@naver.com>
2022-04-21 12:59:29 +0900

    0421 오전수업 - AOP

--- src/main/java/com/mycompany/webapp/dao/mybatis/Ch14MemberDao.java
+++ src/main/java/com/mycompany/webapp/dao/mybatis/Ch14MemberDao.java
@@ -1,11 +1,25 @@
 package com.mycompany.webapp.dao.mybatis;
 
-import org.apache.ibatis.annotations.Mapper;
+import javax.annotation.Resource;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
 
 import com.mycompany.webapp.dto.Ch14Member;
 
-@Mapper
-public interface Ch14MemberDao {
-	public int insert(Ch14Member member);
-	public Ch14Member selectByMid(String mid);
+@Repository
+public class Ch14MemberDao {
+	
+	@Resource
+	private SqlSessionTemplate sqlSessionTemplate;
+	
+	// 프로그램적인 방법
+	public int insert(Ch14Member member) {
+		int rows = sqlSessionTemplate.insert("com.mycompany.webapp.dao.mybatis.Ch14MemberDao.insert", member);
+		return rows;
+	}
+	public Ch14Member selectByMid(String mid) {
+		Ch14Member ch14Member = sqlSessionTemplate.selectOne("com.mycompany.webapp.dao.mybatis.Ch14MemberDao.selectByMid", mid);
+		return ch14Member;
+	}
 }
